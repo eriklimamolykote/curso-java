f30c593d5ef0f5d7fdc46d2a4f3cab2e019785cb
eriklimamolykote <eriklim@hotmail.com>
2021-06-07 10:25:14 -0300

    primeiro-commit-curso-java-07-06-2021

---
+++ campo-minado/src/br/com/cod3r/cm/excecao/ExplosaoException.java
@@ -0,0 +1,12 @@
+// Original author: Leonardo Moura Leitão & Cod3r Cursos
+// (C) 2019, 2021 by Cod3r Cursos. All Rights Reserved
+
+package br.com.cod3r.cm.excecao;
+
+// Classe que gera uma exceção no jogo caso 
+// uma mina exploda
+public class ExplosaoException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+}
--- campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -7,6 +7,8 @@ package br.com.cod3r.cm.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.cod3r.cm.excecao.ExplosaoException;
+
 public class Campo {
 	
 	// Linhas e colunas do campo minado
@@ -26,6 +28,77 @@ public class Campo {
 	}
 	
 	boolean adicionarVizinho(Campo vizinho) {
+		boolean linhaDiferente = linha != vizinho.linha;
+		boolean colunaDiferente = coluna != vizinho.coluna;
+		boolean diagonal = linhaDiferente && colunaDiferente;
 		
+		int deltaLinha = Math.abs(linha - vizinho.linha);
+		int deltaColuna = Math.abs(coluna - vizinho.coluna);
+		int deltaGeral = deltaColuna + deltaLinha;
+		
+		if (deltaGeral == 1 && !diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else if (deltaGeral == 2 && diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	// Método para alternar marcação de locais onde 
+	// provavelmente existe uma mina explosiva
+	void alternarMarcacao() {
+		if (!aberto) {
+			marcado = !marcado;
+		}
+	}
+	
+	// Método para abrir os espaços no campo minado
+	boolean abrir() {
+		
+		if (!aberto && !marcado) {
+			aberto = true;
+			
+			// Chama ExplosaoException se o usuário pisar
+			// na mina explosiva
+			if (minado) {
+				throw new ExplosaoException();
+			}
+			
+			if (vizinhancaSegura()) {
+				vizinhos.forEach(v -> v.abrir());
+			}
+			
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	// Método que verifica se os vizinhos do espaço aberto
+	// por uma mina que já explodiu são seguros
+	boolean vizinhancaSegura() {
+		return vizinhos.stream().noneMatch(v -> v.minado);
+	}
+	
+	/// Insere as minas
+	void minar() {
+		minado = true;
+	}
+	
+	// Métodos que verificam se há uma posição marcada no
+	// campo minado
+	public boolean isMarcado() {
+		return marcado;
+	}
+	
+	public boolean isAberto() {
+		return aberto;
+	}
+	
+	public boolean isFechado() {
+		return !isAberto();
 	}
 }
